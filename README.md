0d8facb46d3c4e9b54acdd5741ec10ec0f698740
WHYNTER-23 <whynter1945@gmail.com>
2023-05-08 18:33:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# St.Gloriana-web-mark-1
---
+++ script.js
@@ -0,0 +1,7 @@
+const redirYoutobe = () => {
+  window.location.href = "https://www.youtube.com";
+};
+
+const redirWhatapps = () => {
+  window.location.href = "https://wa.me/082319232023";
+};
